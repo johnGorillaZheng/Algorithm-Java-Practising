7df62077a444426f504b573c476c9a842fc7bdc7
John Zixuan Zheng <u6153124@anu.edu.au>
2017-06-02 22:27:33 +1000

    my first commit
    my first commit

---
+++ src/Testing.java
@@ -0,0 +1,16 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double[] testArray = new double[100];
+		
+		for(int i = 0; i < 100; i ++){
+			testArray[i] = Math.random();
+			System.out.println(testArray[i]);
+		}
+
+	}
+
+}
