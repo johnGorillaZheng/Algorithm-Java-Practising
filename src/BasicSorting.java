f33378a573621d1e3dadccce62f21ba709405f4c
John Zixuan Zheng <u6153124@anu.edu.au>
2017-06-02 23:29:00 +1000

    sort
    sort

--- src/BasicSorting.java
+++ src/BasicSorting.java
@@ -1,6 +1,66 @@
 
 public class BasicSorting {
+	/**
+	 * 冒泡排序	BubbleSort
+	 * 郑子旋		Zixuan Zheng
+	 * 2017年6月2日
+	 */
+	public static void bubbleSort(double[] arr){
+		int len = arr.length;
+		for(int i = 0; i < len - 1; i ++){
+			for(int j = 0; j < len - 1 - i; j++){
+				if(arr[j] > arr[j+1]){
+					exchange(arr,j,j+1);
+				}
+			}
+		}
+	}
 	
-
+	/**
+	 * 选择排序	selectSort
+	 * 郑子旋	    Zixuan Zheng 
+	 * 2017年6月2日
+	 */	
+	public static void selectSort(double[] arr){
+		int len = arr.length;
+		for(int i = 0; i < len; i++){
+			int min = i;
+			for(int j = i + 1; j < len; j++){
+				if(arr[j] < arr[min]){
+					min = j;
+				}
+			}
+			exchange(arr,i,min);
+		}	
+	}
+	
+	/**
+	 * 插入排序
+	 * 郑子旋
+	 * 2017年6月2日
+	 */
+	public static void insertSort(double[] arr){
+		int len = arr.length;
+		for(int i = 0; i < len - 1; i++){
+			for(int j = i+1; j >0; j--){
+				if(arr[j-1]<arr[j]){
+					break;
+				}
+				exchange(arr,j-1,j);
+			}
+		}
+	}
+	
+	/**
+	 * 交换两个数位置
+	 * 郑子旋
+	 * 2017年6月2日
+	 */
+	public static void exchange(double[] arr,int a, int b){
+		double mid;
+		mid = arr[a];
+		arr[a] = arr[b];
+		arr[b] = mid;
+	}
 
 }
--- src/Testing.java
+++ src/Testing.java
@@ -3,14 +3,19 @@ public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		double[] testArray = new double[100];
-		
-		for(int i = 0; i < 100; i ++){
+		double a = 1, b = 2;
+		double[] testArray = new double[10000];
+//		BasicSorting.exchange(a, b);
+//		System.out.println(a);
+		for(int i = 0; i < testArray.length; i ++){
 			testArray[i] = Math.random();
+		}
+		
+		BasicSorting.selectSort(testArray);
+		
+		for(int i = 0; i < testArray.length; i ++){
 			System.out.println(testArray[i]);
 		}
-
 	}
 
 }
