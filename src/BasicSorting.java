91e7cf08606e78a2f5d257aae8f6198dfa5d4e8f
johngorillazheng <zhengzixuangood@gmail.com>
2018-03-04 15:52:48 +1100

    haha

--- src/BasicSorting.java
+++ src/BasicSorting.java
@@ -52,8 +52,8 @@ public class BasicSorting {
 	public static void insertSort(int[] arr){
 		int len = arr.length;
 		for(int i = 0; i < len - 1; i++){
-			for(int j = i+1; j >0; j--){
-				if(arr[j-1]<arr[j]){
+			for(int j = i + 1; j > 0; j --){
+				if(arr[j-1] < arr[j]){
 					break;
 				}
 				exchange(arr,j-1,j);
